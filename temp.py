acc2ac8880b681be9648615315bca433f8a9179d
Joe <nathanielbandi636@gmail.com>
2025-10-04 09:51:07 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -10,3 +10,8 @@ elif 20 <= temp <= 30:
     print("It's Normal ")
 else:
     print("It's Hot ")
+        temp_fahrenheit = (temp_celsius * 9/5) + 32
+
+    print(f"\nTemperature in Celsius: {temp_celsius}°C")
+print(f"Temperature in Fahrenheit: {temp_fahrenheit}°F")
+print("Condition:", condition)
